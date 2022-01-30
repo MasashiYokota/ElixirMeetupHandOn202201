984eaa77a612fabf574e4986a3ecd3655be901df
Masashi Yokota <taf_masashi@me.com>
2022-01-30 14:17:36 +0900

    Introduce credo

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule Demo.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
